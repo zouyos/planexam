615617aad05c7d23383488a31d5ea8a08149c7b7
zouyos <hamza.benketaf@gmail.com>
2023-09-04 15:24:24 +0200

    small fixes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,4 @@
 insert into role (nom) values ('admin');
 insert into role (nom) values ('prof');
 
-insert into utilisateur (role_id, email, mdp) VALUES (1,'admin@email.com','$2a$10$uSwACi0Xkw9pOVSdFGhzPebFLBqAHDO5hVHr27qVd71OtVDJnuDJS');
-insert into utilisateur (role_id, email, mdp) values (2,'prof@email.com','$2a$10$GM8MfrVFZ3BnjuXJbGYcSu.FsRUnyuHJyVB7es1qU.5mlk1EHp29m');
-
--- mdp admin = admin@email.com1
--- mdp prof = prof@email.com1
+insert into utilisateur (role_id, email, mdp) VALUES (1,'hamza.benketaf@gmail.com','$2a$10$ACvV7pSw.9iPB9ljJFyRZ.Gznr5riJzYulBxKr0A3/ldBNwB4aRBe');
