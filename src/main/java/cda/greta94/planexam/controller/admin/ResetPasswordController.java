a3daae30c509f781806a55aa1b942500aafe9ed7
zouyos <hamza.benketaf@gmail.com>
2023-10-13 10:32:41 +0200

    small fixes

--- src/main/java/cda/greta94/planexam/controller/admin/ResetPasswordController.java
+++ src/main/java/cda/greta94/planexam/controller/admin/ResetPasswordController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ResetPasswordController {
@@ -46,7 +47,7 @@ public class ResetPasswordController {
      * @return
      */
     @PostMapping("/forgot-password")
-    public String processForgotPassword(@RequestParam("email") String email) {
+    public String processForgotPassword(@RequestParam("email") String email, RedirectAttributes redirectAttributes) {
         Utilisateur utilisateur = utilisateurService.findByEmail(email);
         if (utilisateur != null) {
             // Générez un jeton de réinitialisation de mot de passe
@@ -54,22 +55,24 @@ public class ResetPasswordController {
             // Envoyez le jeton au courriel de l'utilisateur et incluez un lien pour réinitialiser le mot de passe
             javaSenderMail.sendPasswordResetEmail(utilisateur.getEmail(), token);
         }
+        redirectAttributes.addFlashAttribute("successMessage", "Email envoyé avec succès");
         return "redirect:/forgot-password?success";
     }
 
     @GetMapping("/reset-password")
-    public String showResetPasswordForm(@RequestParam("token") String token, Model model) {
+    public String showResetPasswordForm(@RequestParam("token") String token, Model model, RedirectAttributes redirectAttributes) {
         JetonResetMdp resetToken = tokenService.findByToken(token);
         if (resetToken != null) {
             PasswordDto dto = new PasswordDto(token);
             model.addAttribute("passwordDto", dto);
             return "admin/security/resetMdp";
         }
+        redirectAttributes.addFlashAttribute("errorMessage", "Token invalide");
         return "redirect:/forgot-password?invalidToken";
     }
 
     @PostMapping("/reset-password")
-    public String processResetPassword(@Valid @ModelAttribute PasswordDto passwordDto, BindingResult bindingResult)  {
+    public String processResetPassword(@Valid @ModelAttribute PasswordDto passwordDto, BindingResult bindingResult, RedirectAttributes redirectAttributes)  {
         if (bindingResult.hasErrors()) {
             return "admin/security/resetMdp";
         }
@@ -79,8 +82,10 @@ public class ResetPasswordController {
             Utilisateur utilisateur = utilisateurService.convertToEntity(passwordDto);
             utilisateurService.saveNewPassword(utilisateur);
             tokenService.deleteToken(resetToken);
+            redirectAttributes.addFlashAttribute("successMessage", "Votre mot de passe a bien été modifié");
             return "redirect:/login?passwordResetSuccess";
         }
+        redirectAttributes.addFlashAttribute("errorMessage", "Erreur lors du renouvellement du mot de passe");
         return "redirect:/login?passwordResetError";
     }
 }
